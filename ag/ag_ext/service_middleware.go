e92920c614890ed2e471cf8eadc63bd421df2610
frochy <frochyzhang@gmail.com>
2025-07-02 21:08:36 +0800

    service中间件调整

--- ag/ag_ext/service_middleware.go
+++ ag/ag_ext/service_middleware.go
@@ -6,6 +6,8 @@ import (
 	"time"
 )
 
+type HandlerFunc func(ctx context.Context, req interface{}) (interface{}, error)
+
 // Middleware 中间件类型
 type Middleware func(
 	method string,
@@ -13,6 +15,21 @@ type Middleware func(
 	req interface{},
 	next func(context.Context, interface{}) (interface{}, error),
 ) (interface{}, error)
+
+// 注册单个方法的处理链
+func registerHandler(proxy interface{}, methodName string, middlewares []Middleware, handler HandlerFunc) HandlerFunc {
+	// 从后向前包装中间件
+	wrappedHandler := handler
+	for i := len(middlewares) - 1; i >= 0; i-- {
+		mw := middlewares[i]
+		next := wrappedHandler
+		wrappedHandler = func(ctx context.Context, req interface{}) (interface{}, error) {
+			return mw(methodName, ctx, req, next)
+		}
+	}
+
+	return wrappedHandler
+}
 
 // LoggingMiddleware 示例：日志中间件
 func LoggingMiddleware(
